97fd18ad015d861d870ba5ccf89017bc9fe396f3
olena-kharchenko <harchenko.es1990@gmail.com>
2021-02-03 17:05:23 +0200

    step 3

--- src/App.js
+++ src/App.js
@@ -6,9 +6,19 @@ import {
   Typography,
   Box,
   Button,
+  Paper,
+  Grid,
+  Card,
+  CardMedia,
+  CardContent,
+  CardActions,
 } from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
+import LayerIcon from '@material-ui/icons/Layers';
+import PlayCircleFilledIcon from '@material-ui/icons/PlayCircleFilled';
 import { makeStyles } from '@material-ui/core/styles';
+
+const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -19,6 +29,37 @@ const useStyles = makeStyles(theme => ({
   },
   title: {
     flexGrow: 1,
+  },
+  overlay: {
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    right: 0,
+    left: 0,
+    backgroundColor: 'rgba(0,0,0,.3)',
+  },
+  mainFeaturesPost: {
+    position: 'relative',
+    color: theme.palette.common.white,
+    marginBottom: theme.spacing(4),
+
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'center',
+  },
+  mainFeaturesPostContent: {
+    position: 'relative',
+    padding: theme.spacing(6),
+    marginTop: theme.spacing(8),
+  },
+  cardMedia: {
+    paddingTop: '56.25%',
+  },
+  cardContent: {
+    flexGrow: 1,
+  },
+  cardGrid: {
+    marginTop: theme.spacing(4),
   },
 }));
 
@@ -54,6 +95,113 @@ export default function App() {
           </Toolbar>
         </Container>
       </AppBar>
+
+      <main>
+        <Paper
+          className={classes.mainFeaturesPost}
+          style={{ backgroundImage: `url(https://source.unsplash.com/random)` }}
+        >
+          <Container fixed>
+            <div className={classes.overlay} />
+            <Grid container>
+              <Grid item md={6}>
+                <div className={classes.mainFeaturesPostContent}>
+                  <Typography
+                    component="h1"
+                    variant="h3"
+                    color="inherit"
+                    gutterBottom
+                  >
+                    Web Developer Blog
+                  </Typography>
+                  <Typography variant="h5" color="inherit" paragraph>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua. Nulla facilisi etiam dignissim diam quis enim
+                    lobortis scelerisque fermentum. Congue eu consequat ac felis
+                    donec et odio pellentesque.
+                  </Typography>
+                  <Button color="secondary" variant="contained">
+                    Learn more
+                  </Button>
+                </div>
+              </Grid>
+            </Grid>
+          </Container>
+        </Paper>
+        <div className={classes.mainContent}>
+          <Container maxWidth="md">
+            <Typography
+              variant="h2"
+              align="center"
+              color="textPrimary"
+              gutterBottom
+            >
+              Web Developer Blog
+            </Typography>
+            <Typography
+              variant="h5"
+              align="center"
+              color="textSecondary"
+              paragraph
+            >
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Nulla
+              facilisi etiam dignissim diam quis enim lobortis scelerisque
+              fermentum. Congue eu consequat ac felis donec et odio
+              pellentesque.
+            </Typography>
+            <div className={classes.mainButtons}>
+              <Grid container spacing={2} justify="center">
+                <Grid item>
+                  <Button color="primary" variant="contained">
+                    Start now
+                  </Button>
+                </Grid>
+                <Grid item>
+                  <Button color="primary" variant="outlined">
+                    Learn more
+                  </Button>
+                </Grid>
+              </Grid>
+            </div>
+          </Container>
+        </div>
+
+        <Container maxWidth="md" className={classes.cardGrid}>
+          <Grid container spacing={4} justify="center">
+            {cards.map(card => (
+              <Grid item key={card} xs={12} sm={6} md={4}>
+                <Card className={classes.card}>
+                  <CardMedia
+                    className={classes.cardMedia}
+                    image="https://source.unsplash.com/random"
+                    title="Image title"
+                  />
+                  <CardContent className={classes.cardContent}>
+                    <Typography variant="h5" gutterBottom>
+                      Blog post
+                    </Typography>
+                    <Typography variant="h6" gutterBottom>
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Button color="primary" size="small">
+                      View
+                    </Button>
+                    <Button color="primary" size="small">
+                      Edit
+                    </Button>
+                    <LayerIcon />
+                    <PlayCircleFilledIcon />
+                  </CardActions>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+        </Container>
+      </main>
     </>
   );
 }
